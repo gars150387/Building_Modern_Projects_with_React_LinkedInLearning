8675929b6d3da0e22423c21153ef9e7d33c407d1
gars150387 <gar.santeliz@gmail.com>
2021-10-12 14:23:46 +0000

    working on webpack preset babel env changed to react

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/env"],
+          presets: ["@babel/react"],
         },
       },
       {
